3d98c3e077a275d31a757e5274a7186cf19cd38a
zerob13 <i@zerob13.in>
2015-05-15 18:00:24 +0800

    add bin

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
 #!/usr/bin/env node
 
-// var fs = require('fs'),
-  // path = require('path');
-// var pkg = require(path.join(__dirname, 'package.json'));
-var CF = require('coffee-formatter');
-// var program = require('commander');
-// program
-//   .version(pkg.version)
-//   .arguments('[file]')
-//   .action(function(file) {
-//     var CF = require('coffee-formatter');
-//     fs.readFile(file, 'utf8', function(err, data) {
-//       if (err) {
-//         return console.log(err);
-//       }
-//       var CF = require('coffee-formatter');
-//       var lines = data.split('\n');
-//       var resultArr = [];
-//       for (var i = 0; i < lines.length; i++) {
-//         var curr=lines[i];
-//         var p = CF.formatTwoSpaceOperator(curr);
-//         p = CF.formatOneSpaceOperator(p);
-//         p = CF.shortenSpaces(p);
-//         resultArr.push(p);
-//       }
-//       result = resultArr.join('\n');
-//       console.log(result);
-//     });
-//   })
+require('coffee-formatter');
 
-// program.parse(process.argv);
