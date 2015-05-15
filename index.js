ae82397a543e8df557e5656db1564c56900bd4a5
zerob13 <i@zerob13.in>
2015-05-15 19:07:59 +0800

    modify package

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
 #!/usr/bin/env node
 
-require('coffee-formatter');
+var fs = require('fs');
+var path = require('path');
+var program = require('commander');
+var pkg = require(path.join(__dirname, 'package.json'));
 
+program
+  .version(pkg.version)
+  .arguments('<file>')
+  .action(function(file) {
+    file = file;
+
+  });
+program.parse(process.argv);
+var data = '';
+var CF = require('./formatter');
+
+if (typeof file === 'undefined') {
+  input = process.stdin;
+  input.resume();
+  input.setEncoding('utf8');
+  input.on('data', function(chunk) {
+    data += chunk;
+  });
+  input.on('end', function() {
+    formatIt(data);
+  });
+
+} else {
+  var text = fs.readFileSync(file, 'utf8');
+  formatIt(text);
+}
+
+function formatIt(text) {
+  var lines = text.split('\n');
+  var resultArr = [];
+  for (var i = 0; i < lines.length; i++) {
+    var curr = lines[i];
+    var p = CF.formatTwoSpaceOperator(curr);
+    p = CF.formatOneSpaceOperator(p);
+    p = CF.shortenSpaces(p);
+    resultArr.push(p);
+  }
+  var result = resultArr.join('\n');
+  process.stdout.write(result);
+  process.exit(0);
+}
